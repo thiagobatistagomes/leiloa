a0c3bc2ac40a693a470887fde2f74818c64fb51e
Thiago <thiagobg1811@gmail.com>
2026-01-06 13:57:04 -0300

    back feat:first commit

---
+++ backend/leiloa-api/src/main/java/com/example/leiloa_api/LeiloaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.leiloa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiloaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiloaApiApplication.class, args);
+	}
+
+}
---
+++ backend/leiloa-api/src/test/java/com/example/leiloa_api/LeiloaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leiloa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiloaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
